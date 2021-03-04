ca823da3d8c323b634e98446c8b1e224c51c796d
zplusfour <zplusfour.node@outlook.com>
2021-03-04 11:30:48 +0200

    added a simple golang ssh server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/gliderlabs/ssh"
+)
+
+func main() {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
+	})
+
+	log.Println("starting ssh server on port 3000...")
+	log.Fatal(ssh.ListenAndServe(":3000", nil))
+}
