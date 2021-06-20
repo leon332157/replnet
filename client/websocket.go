1623b49fb29bfb435df7eb11a918227d1bcee48e
leon332157 <leon332157@gmail.com>
2021-06-20 00:44:38 -0400

    fix websocket demo

--- client/websocket.go
+++ client/websocket.go
@@ -2,19 +2,26 @@ package client
 
 import (
 	"context"
-	"time"
+	"fmt"
 	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
+	"time"
 )
 
 func StartWS() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-defer cancel()
+	defer cancel()
 
-c, _, err := websocket.Dial(ctx, "ws://localhost:8080", nil)
-if err != nil {
-	// ...
-}
-defer c.Close(websocket.StatusInternalError, "the sky is falling")
+	c, _, err := websocket.Dial(ctx, "ws://localhost:8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer c.Close(websocket.StatusInternalError, "the sky is falling")
 
-c.Close(websocket.StatusNormalClosure, "")
+	err = wsjson.Write(ctx, c, "hi")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.Close(websocket.StatusNormalClosure, "")
 }
--- server/websocket.go
+++ server/websocket.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"nhooyr.io/websocket"
+	"context"
+	"nhooyr.io/websocket/wsjson"
+	"time"
+)
+
+func handleWS(w http.ResponseWriter, r *http.Request) {
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		// ...
+	}
+	defer c.Close(websocket.StatusInternalError, "the sky is falling")
+
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
+	defer cancel()
+
+	var v interface{}
+	err = wsjson.Read(ctx, c, &v)
+	if err != nil {
+		// ...
+	}
+
+	log.Printf("received: %v", v)
+
+	c.Close(websocket.StatusNormalClosure, "")
+}
