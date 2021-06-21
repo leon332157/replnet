94cb01b37f9f6ea6cf0caf9fb6f6c2142530701f
leon332157 <leon332157@gmail.com>
2021-06-21 16:38:18 -0400

    ws ping not working

--- client/websocket.go
+++ client/websocket.go
@@ -2,28 +2,54 @@ package client
 
 import (
 	"context"
-	"fmt"
 	log "github.com/sirupsen/logrus"
 	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/wsjson"
 	"time"
 )
 
+func heartbeat(ctx context.Context, c *websocket.Conn, d time.Duration) {
+	t := time.NewTimer(d)
+	defer t.Stop()
+	for {
+		log.Debugln("a")
+		select {
+		case <-ctx.Done():
+			log.Debugln("done")
+			return
+		case <-t.C:
+		}
+		err := c.Ping(ctx)
+		if err != nil {
+			log.Debugln(err)
+		} else {
+			log.Debugln("Ping!")
+		}
+
+		t.Reset(time.Second)
+	}
+}
 func StartWS() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
 	c, _, err := websocket.Dial(ctx, "ws://localhost:7070/__ws", nil)
 	if err != nil {
-		log.Errorf("[Websocket Client] Dial failed: %s", err)
+		log.Fatalf("[Websocket Client] Dial failed: %s", err)
 	}
 
-	defer c.Close(websocket.StatusInternalError, "the sky is falling")
-
-	err = wsjson.Write(ctx, c, "hi")
-	c.Ping(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	c.Close(websocket.StatusNormalClosure, "")
+	//defer c.Close(websocket.StatusInternalError, "the sky is falling")
+	/*go func() {
+		for {
+			timeout, _ := context.WithTimeout(context.Background(), 5*time.Second)
+			err := c.Ping(timeout)
+			log.Debugln("[Websocket Client] Keep alive")
+			if err != nil {
+				log.Debugf("[Websocket Client] Keep alive err: %s\n", err)
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()*/
+	hb := context.TODO()
+	go heartbeat(hb, c, time.Second)
+	//c.Close(websocket.StatusNormalClosure, "")
 }
--- server/websocket.go
+++ server/websocket.go
@@ -1,31 +1,34 @@
 package server
 
 import (
+	"context"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"nhooyr.io/websocket"
-	"context"
-	"nhooyr.io/websocket/wsjson"
-	"time"
 )
+func UNUSED(x ...interface{}) {
+}
+
 
 func handleWS(w http.ResponseWriter, r *http.Request) {
 	c, err := websocket.Accept(w, r, nil)
 	if err != nil {
-		// ...
+		log.Errorf("[Websocker Handler] Accept err: %v\n",err)
 	}
-	defer c.Close(websocket.StatusInternalError, "the sky is falling")
-
-	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
-	defer cancel()
-
-	var v interface{}
-	err = wsjson.Read(ctx, c, &v)
-	if err != nil {
-		// ...
-	}
-
-	log.Printf("received: %v", v)
-
-	c.Close(websocket.StatusNormalClosure, "")
+	UNUSED(c)
+	/*go func() {
+		for {
+			timeout, _ := context.WithTimeout(context.Background(), 5*time.Second)
+			err := c.Ping(timeout)
+			log.Debugln("[Websocket Handler] Keep alive")
+			if err != nil {
+				log.Debugf("[Websocket Handler] Keep alive err: %s\n", err)
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()*/
+	_, data, err := c.Read(context.Background())
+	log.Debugf("[WS handler] data: %v, err: %v",data,err)
+	//c.Close(websocket.StatusNormalClosure, "")
 }
