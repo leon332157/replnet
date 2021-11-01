82f79dc4f07821e5b2ef53c11531c957598e445d
leon332157 <leon332157@gmail.com>
2021-11-01 09:50:54 -0400

    fix scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,2 @@
-go build -ldflags="-s -w" -o replish
+#!/bin/bash
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o replish-linux-amd64
--- scripts/install_replit.sh
+++ scripts/install_replit.sh
@@ -4,7 +4,7 @@ if [ -z ${REPL_SLUG} ];
 then
     cd /home/runner/$REPL_SLUG
 fi
-curl https://github.com/leon332157/replish/releases/latest/download/replish-linux-amd64 -# --compressed -o replish
+curl https://github.com/ReplDepot/replish/releases/latest/download/replish-linux-amd64 -# --compressed -o replish
 chmod +x replish
 
 function write_exmaple_config() {
