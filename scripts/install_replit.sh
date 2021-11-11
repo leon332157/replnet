66e3ee26160e02a2c137d91279e12f2a7c2d1070
leon332157 <leon332157@gmail.com>
2021-11-11 13:49:03 -0500

    fix script

--- scripts/install_replit.sh
+++ scripts/install_replit.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
-echo "Downloading replish binary to /home/runner/{$REPL_SLUG}"
-if [ -z $REPL_SLUG ];
-then
+if [[ -v ${REPL_SLUG} ]];then
     cd /home/runner/$REPL_SLUG
 else
     echo "Script must be run in a repl"
     exit 1
 fi
 
+echo "Downloading replish binary to /home/runner/$REPL_SLUG"
 curl -fL https://github.com/ReplDepot/replish/releases/latest/download/replish-linux-amd64 -# --compressed -o replish
 chmod +x replish
 
