067056f768f32d338d009e4af30f700d8dc67b37
leon332157 <leon332157@gmail.com>
2021-11-11 14:02:58 -0500

    don't write config file if field exist

--- scripts/install_replit.sh
+++ scripts/install_replit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-if [[ -v ${REPL_SLUG} ]];then
+if [[ -v REPL_SLUG ]];then
     cd /home/runner/$REPL_SLUG
 else
     echo "Script must be run in a repl"
@@ -7,12 +7,19 @@ else
 fi
 
 echo "Downloading replish binary to /home/runner/$REPL_SLUG"
-curl -fL https://github.com/ReplDepot/replish/releases/latest/download/replish-linux-amd64 -# --compressed -o replish
-chmod +x replish
-
+curl -fL https://github.com/ReplDepot/replish/releases/latest/download/replish-linux-amd64 --progress-bar --compressed -o replish
+if [ $? -eq 0 ]; then
+    chmod +x replish
+else
+    echo "Download error"
+    exit 1
+fi
 function write_exmaple_config() {
     echo "[replish]" >> .replit;
     echo 'mode = "server"' >> .replit;
 }
-
-write_exmaple_config
+if ! grep -q "[replish]" ".replit";then
+    write_exmaple_config
+else
+    echo "replish field exist, skipping writing config"
+fi
